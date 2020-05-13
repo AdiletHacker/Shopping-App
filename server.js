9b0257a86553896d73209af5f99e9971f8b421e0
Adilet <adiletbeyshekeev@mail.ru>
2020-05-14 02:34:10 +0600

    make it better

--- server.js
+++ server.js
@@ -1,18 +1,13 @@
 
-if (process.env.NODE_ENV !== "production") {
-    require("dotenv").config();
-}
-
 
 const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
-const Items = require("./routes/api/items");
 const app = express();
 
 
 app.use(express.json());
-app.use("/api/items", Items);
+app.use("/api/items", require("./routes/api/items"));
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
@@ -23,7 +18,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 
-mongoose.connect(process.env.DB_URL, { useUnifiedTopology: true, useNewUrlParser: true });
+mongoose.connect("mongodb+srv://user:UlTWec91AgtUwjFB@cluster0-knb9o.mongodb.net/test?authSource=admin&replicaSet=Cluster0-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass%20Community&retryWrites=true&ssl=true", { useUnifiedTopology: true, useNewUrlParser: true });
 const db = mongoose.connection;
 db.on("error", error => console.error(error));
 db.on("open", () => console.log("Connected to Database!"));
