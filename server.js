59eeeaf43c12d7d4a5014bf71b3844420ae5304a
Adilet <adiletbeyshekeev@mail.ru>
2020-05-13 21:56:33 +0600

    make it better

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
-require("dotenv").config();
+if (process.env.NODE_ENV !== "production") {
+    require("dotenv").config();
+}
 
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const path = require("path");
 const Items = require("./routes/api/items");
 const app = express();
 
 
 app.use(bodyParser.json());
 app.use("/api/items", Items);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+
+    app.get("*", (req, res) => {
+        res.sendFile(__dirname, "client", "build", "index.html");
+    });
+}
 
 
 mongoose.connect(process.env.DB_URL, { useUnifiedTopology: true, useNewUrlParser: true });
@@ -20,7 +31,7 @@ db.on("open", () => console.log("Connected to Database!"));
 
 
 
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 3000);
 
 
 
