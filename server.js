a693af1ad710bdcc29c22d04381b5638d4942164
Adilet <adiletbeyshekeev@mail.ru>
2020-05-14 02:51:58 +0600

    make it better

--- server.js
+++ server.js
@@ -15,10 +15,10 @@ if (process.env.NODE_ENV === "production") {
     app.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
     });
-}
+};
 
 
-mongoose.connect("mongodb+srv://user:UlTWec91AgtUwjFB@cluster0-knb9o.mongodb.net/test?authSource=admin&replicaSet=Cluster0-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass%20Community&retryWrites=true&ssl=true", { useUnifiedTopology: true, useNewUrlParser: true });
+mongoose.connect("mongodb+srv://user:zcH85ABsA0XGyzyw@cluster0-knb9o.mongodb.net/test?retryWrites=true&w=majority");
 const db = mongoose.connection;
 db.on("error", error => console.error(error));
 db.on("open", () => console.log("Connected to Database!"));
